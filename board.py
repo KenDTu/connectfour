e0bd7eeaa960dca7ff467a185f38fb6dcc765014
Ken Tu <kentu@eduroam-campus-10-36-82-52.campus-dynamic.uwaterloo.ca>
2026-07-08 22:04:47 -0400

    deciding on the grid

--- board.py
+++ board.py
@@ -6,13 +6,29 @@ class Board:
     def __init__(self, rows = 6, columns = 7):
         self.rows = rows
         self.columns = columns
+        self.grid = []
+
+        # Make the grid
+        for i in range (0, self.columns):
+            self.grid.append([" "])
 
     # ====== Member Functions to Manage Board State Logic ====== 
+    # Function: 
+    # Input: 
+    # Output: 
 
+
+    # Function: To print the 2D matrix that represents the state of the board
+    # Input: Nothing
+    # Output: Prints the state of the board the console terminal
+    def printBoard() -> None: 
+        pass
+
+        
     # Function: To determine whether a player can place a piece in that column
     # Input: Column integer e.g. 0, 1, ..., 6
     # Output: True or False 
-    def isValidColumn(column):
+    def isValidColumn(column) -> bool:
         pass
 
 
