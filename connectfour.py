36c800c039e746fae5ed23191d996130aeaaccc2
Ken Tu <kenduytu04@gmail.com>
2026-07-24 14:09:56 -0400

    Completed Connect Four Functionality

--- connectfour.py
+++ connectfour.py
@@ -6,7 +6,7 @@ COLUMN_COUNT: int = 7
 
 # Create the Connect Four Board
 def createBoard():
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 # Flips the board so the piece build upwards
@@ -19,7 +19,7 @@ def dropPiece(board, row, col, piece):
 
 # Verifies that the column is a valid location for the piece
 def isValidLocation(board, col):
-    return board[5][col] == 0 #should the row be the 0?
+    return board[ROW_COUNT-1][col] == 0 
 
 # Obtains the next open column by checking each column
 def getNextOpenRow(board, col):
@@ -27,7 +27,31 @@ def getNextOpenRow(board, col):
         if board[r][col] == 0:
             return r
 
-
+def winningMove(board, piece):
+    # Check all the horizontal locations
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+        
+    # Check all vertical locations
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+         
+    # Check positively sloped diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+            
+    # Check negatively sloped diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+            
 board = createBoard()
 print(board)
 gameOver: bool = False
@@ -37,25 +61,32 @@ while not gameOver:
     # Ask for Player 1 Input
     if turn == 0:
         col = int(input("Player 1, make your selection (0-6)"))
-        print(col)
-        print(type(col))
-        
         if isValidLocation(board,col):
             row = getNextOpenRow(board, col)
             dropPiece(board, row, col, 1)
+        
+            if winningMove(board, 1):
+                print("Player 1 wins! Congrats!")
+                gameOver = True
+                break
         else: 
-            print("Not a valid location.")
-
+            turn += 1 # handles invalid placement error
 
     # Ask for Player 2 Input
     else:
         col = int(input("Player 2, make your selection (0-6)"))
-        print(col)
-        print(type(col))
 
         if isValidLocation(board,col):
             row = getNextOpenRow(board, col)
             dropPiece(board, row, col, 2)
+            
+
+            if winningMove(board, 2):
+                print("Player 2 wins! Congrats!")
+                gameOver = True
+                break
+        else: 
+            turn += 1 # handles invalid placement error
 
     printBoard(board)
     turn += 1
