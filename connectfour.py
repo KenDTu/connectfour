b15cc81a106f6d2a9af5044fed4d9f74dfb5a65a
Ken Tu <kenduytu04@gmail.com>
2026-07-24 12:07:33 -0400

    Adding the board

--- connectfour.py
+++ connectfour.py
@@ -1,1 +1,16 @@
 # This program codes for a game of Connect Four on the terminal
+import numpy as np
+
+def createBoard():
+    board = np.zeros((6,7))
+    return board
+
+board = createBoard()
+gameOver: bool = False
+turn: int = 0         # This defines whose turn it is to play 
+
+print(board)
+
+while not gameOver:
+    # Ask for the Player 1 Input
+    
